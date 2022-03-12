7d6e8cff07deb483349a27cd6b2758c6b0f357d1
Danila Ryabov <danila02478@gmail.com>
2022-03-12 22:35:20 +0300

    added constants

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/First.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/First.java
@@ -1,7 +1,17 @@
 package ru.javarush.ryabov.cryptoanalizer;
 
 public class First {
+    public static final String RUSLOW = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    public static final String RUSHIGH = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    public static final String PUNCTUATION = "/?.,'\"}][{;:><_-+=|\\/)(*?&^%$№#@!~`";
+    public static final String ENGLOW = "abcdefghijklmnopqrstuvwxyz";
+    public static final String ENGHIGH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String NUMBERS = "0123456789";
+
+
+    public static final String CONSTANTS = RUSLOW+RUSHIGH+PUNCTUATION+ ENGLOW + ENGHIGH + NUMBERS;
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        System.out.println(CONSTANTS);
     }
 }
