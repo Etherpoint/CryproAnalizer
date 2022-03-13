eae91b0a458c5bde7cd1b01f911c27d0e3a1cc77
Danila Ryabov <danila02478@gmail.com>
2022-03-13 16:49:55 +0300

    added signs in Constants clacc

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
@@ -2,7 +2,7 @@ package ru.javarush.ryabov.cryptoanalizer.constants;
 
 public class Constants {
     private static final String RUS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-    private static final String PUNCTUATION = "/?.,'}][{;:><_-+=|)(*?&^%$№#@!~` ";
+    private static final String PUNCTUATION = "/?.,'}][{;:><_-+=|)(*?&^%$№#@!~` «»—…";
     private static final String ENG = "abcdefghijklmnopqrstuvwxyz";
     private static final String NUMBERS = "0123456789";
     private static final String SHIELDING = "\t\n\"\\\b\r\f";
