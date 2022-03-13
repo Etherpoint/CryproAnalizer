c0a17d96e74b5fe33346859a3af0da1f49e99271
Danila Ryabov <danila02478@gmail.com>
2022-03-13 17:33:27 +0300

    added AnalizerConstants class

---
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/AnalizerConstants.java
@@ -0,0 +1,6 @@
+package ru.javarush.ryabov.cryptoanalizer.constants;
+
+public class AnalizerConstants {
+    public static final String RUSVOWELS = "аеёийоуыэюя";
+    public static final String RUSCONSONANTS = "бвгджзклмнпрстфхцчшщ";
+}
---
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/keyanalizer/KeyAnalizer.java
@@ -0,0 +1,14 @@
+package ru.javarush.ryabov.cryptoanalizer.keyanalizer;
+
+import ru.javarush.ryabov.cryptoanalizer.constants.AnalizerConstants;
+import ru.javarush.ryabov.cryptoanalizer.constants.Constants;
+
+public class KeyAnalizer {
+    public static int keyAnalizer (String text) {
+
+        for (int i = 0; i < text.length(); i++) {
+            char character = text.charAt(i);
+        }
+        return 0;
+    }
+}
