c5f3ddbea1eaa2179a3c0541bdf3d28d135a7da6
Danila Ryabov <danila02478@gmail.com>
2022-03-14 19:44:16 +0300

    modified Constants class

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
@@ -2,13 +2,14 @@ package ru.javarush.ryabov.cryptoanalizer.constants;
 
 public class Constants {
     private static final String RUS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-    private static final String PUNCTUATION = "/?.,'}][{;:><_-+=|)(*?&^%$№#@!~` «»—…";
+    private static final String PUNCTUATION = "/?.,'}][{;:><_-+=|)(*&^%$№#@!~` «»—…";
+    private static final String RUPUNCTUATION = "?.,;:><_-+=()*^%#@№!«»—…";
     private static final String ENG = "abcdefghijklmnopqrstuvwxyz";
     private static final String NUMBERS = "0123456789";
     private static final String SHIELDING = "\t\n\"\\\b\r\f";
-    private static final String STRINGCONSTANTS = RUS + RUS.toUpperCase() + PUNCTUATION + ENG + ENG.toUpperCase()+ SHIELDING + NUMBERS;
+    public static final String STRINGCONSTANTS = RUS + RUS.toUpperCase() + PUNCTUATION + ENG + ENG.toUpperCase()+ SHIELDING + NUMBERS;
+
+    public static final String RUSCONST = RUS + RUS.toUpperCase() + RUPUNCTUATION;
 
     public static final int KEY = 10;
-
-    public static final char[] CONSTANTS = STRINGCONSTANTS.toCharArray();
 }
