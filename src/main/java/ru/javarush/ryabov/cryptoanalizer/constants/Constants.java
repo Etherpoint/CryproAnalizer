69d4bb90f0ccc2079e1af87625c890490675dd93
Danila Ryabov <danila02478@gmail.com>
2022-03-15 17:13:59 +0300

    modified Constants class

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
@@ -4,13 +4,10 @@ public class Constants {
     private static final String RUS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     private static final String RUPUNCTUATION = "?.,;:><_-+=()*^%#@№!«»—…";
     private static final String NUMBERS = "0123456789";
-    private static final String SHIELDING = "\t\n\"\b\r\f";
-    private static final String STRINGCONSTANTS = RUS + RUS.toUpperCase() + RUPUNCTUATION + SHIELDING + NUMBERS;
 
     public static final String CONSONANTS = "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФЧЦЧШЩ";
     public static final String RUSCONST = RUS + RUS.toUpperCase() + RUPUNCTUATION + NUMBERS;
     public static final char[] CHAR_RUCONST = RUSCONST.toCharArray();
-    public static final char[] CONSTANTS = STRINGCONSTANTS.toCharArray();
 
     public static final int KEY = 10;
 }
