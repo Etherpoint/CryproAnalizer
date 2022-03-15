568b9e9caecc6875308531aa42386e79a10e4672
Danila Ryabov <danila02478@gmail.com>
2022-03-15 17:13:05 +0300

    modified Constants class

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
@@ -2,13 +2,10 @@ package ru.javarush.ryabov.cryptoanalizer.constants;
 
 public class Constants {
     private static final String RUS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-    private static final String PUNCTUATION = "/?.,'}][{;:><_-+=|)(*&^%$№#@!~` «»—…";
     private static final String RUPUNCTUATION = "?.,;:><_-+=()*^%#@№!«»—…";
-    private static final String ENG = "abcdefghijklmnopqrstuvwxyz";
     private static final String NUMBERS = "0123456789";
-    private static final String SHIELDING = "\t\n\"\\\b\r\f";
-    private static final String STRINGCONSTANTS = RUS + RUS.toUpperCase() + PUNCTUATION + ENG + ENG.toUpperCase() + SHIELDING + NUMBERS;
-
+    private static final String SHIELDING = "\t\n\"\b\r\f";
+    private static final String STRINGCONSTANTS = RUS + RUS.toUpperCase() + RUPUNCTUATION + SHIELDING + NUMBERS;
 
     public static final String CONSONANTS = "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФЧЦЧШЩ";
     public static final String RUSCONST = RUS + RUS.toUpperCase() + RUPUNCTUATION + NUMBERS;
