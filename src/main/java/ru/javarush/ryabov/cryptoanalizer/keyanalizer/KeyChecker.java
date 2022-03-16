d3f85b2208c54c31ea451212faea4b56dda49bb5
Danila Ryabov <danila02478@gmail.com>
2022-03-16 23:30:05 +0300

    class at work

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/keyanalizer/KeyChecker.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/keyanalizer/KeyChecker.java
@@ -4,7 +4,8 @@ import ru.javarush.ryabov.cryptoanalizer.constants.AnalizerConstants;
 import ru.javarush.ryabov.cryptoanalizer.constants.Constants;
 
 public class KeyChecker {
-    public static void keyChecker(String result) {
+    public static boolean keyChecker(String result) {
+        boolean res = false;
         for (int i = 0; i < result.length(); i++) {
             char character = result.charAt(i);
 
@@ -12,12 +13,18 @@ public class KeyChecker {
             if (contains(Constants.CONSONANTS, String.valueOf(character))){
                 if (contains(Constants.CONSONANTS, String.valueOf(result.charAt(i+1)))){
                     if (contains(Constants.CONSONANTS, String.valueOf(result.charAt(i+2)))){
-                        System.out.println("Невалидный ключ");
                         break;
                     }
                 }
             }
+
+            //Условие, если символ проходит проверку
+            else{
+                res = true;
+            }
         }
+        System.out.println("Возможная расшифровка: " + result);
+        return res;
     }
 
     public static boolean contains(String str, String substr) {
