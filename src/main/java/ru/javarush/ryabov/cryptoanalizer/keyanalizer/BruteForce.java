dd17a352f144d13b0012d06c7dd3c7f6034d169e
Danila Ryabov <danila02478@gmail.com>
2022-03-16 23:29:35 +0300

    finished class

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/keyanalizer/BruteForce.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/keyanalizer/BruteForce.java
@@ -1,7 +1,31 @@
 package ru.javarush.ryabov.cryptoanalizer.keyanalizer;
 
-public class BruteForce {
-    public static void bruteForce (String text){
+import ru.javarush.ryabov.cryptoanalizer.constants.Constants;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class BruteForce {
+    public static void bruteForce(String text) {
+        //Собираем уникальные символы в сет
+        String copyText = new String (text);
+        HashSet<Character> unique = new HashSet<>();
+        for (char character : text.toCharArray()) {
+            unique.add(character);
+        }
+        //Проходимся по сету и меняем символы в тексте
+        do {
+            for (char character : unique) {
+                ArrayList<Character> copy = new ArrayList<>((Constants.RUCONST));
+                int index = (int) (Math.random() * copy.size());
+                char randomizer = copy.get(index);
+                copyText.replace(character, randomizer);
+                copy.remove((Object) randomizer);
+            }
+        }while (!(KeyChecker.keyChecker(copyText)));{
+            copyText = null;
+        }
     }
+
 }
