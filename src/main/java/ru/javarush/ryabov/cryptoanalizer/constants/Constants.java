9fee9cedc55ab6927c37a5443345ee7fecbcba49
Danila Ryabov <danila02478@gmail.com>
2022-03-17 16:57:54 +0300

    added new global constants

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/constants/Constants.java
@@ -4,9 +4,12 @@ import java.util.*;
 
 public class Constants {
     public static final String CONSONANTS = "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФЧЦЧШЩ";
-    public static final List<Character> RUCONST = new ArrayList<>(Arrays.asList('а','б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н',
+    public static final List<Character> RU_CONST = new ArrayList<>(Arrays.asList('а','б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н',
             'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж',
             'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', '0',
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '?', '.', ',', ';', ':', '>', '<', '_', '-', '+', '=', '(', ')', '*', '^', '%', '#',
             '@', '№', '!', '«', '»', '—', '…', ' '));
+    public static final String PUNCTUATION = "?.,;:=%№!… ";
+    public static final String RU_ALPHABET = "АБВГДЕЁЖЗИЁКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    public static final String RU_LOW_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 }
