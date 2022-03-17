aafcba0b9719fc6ef227b55c48d016e84bac3d08
Danila Ryabov <danila02478@gmail.com>
2022-03-17 17:00:59 +0300

    New conditions added

--- src/main/java/ru/javarush/ryabov/cryptoanalizer/keyanalizer/KeyChecker.java
+++ src/main/java/ru/javarush/ryabov/cryptoanalizer/keyanalizer/KeyChecker.java
@@ -1,30 +1,65 @@
 package ru.javarush.ryabov.cryptoanalizer.keyanalizer;
 
-import ru.javarush.ryabov.cryptoanalizer.constants.AnalizerConstants;
 import ru.javarush.ryabov.cryptoanalizer.constants.Constants;
 
 public class KeyChecker {
-    public static boolean keyChecker(String result) {
-        boolean res = false;
+    public static int keyChecker(String result) {
+        //Условие наличия заглавных букв ЪЬЫ
+        if (contains(result, "Ъ") || contains(result, "Ь") || contains(result, "Ы")){
+            return 1;
+        //Условие проверки первого символа
+        }else if (contains("»,>;:)." + Constants.RU_LOW_ALPHABET, String.valueOf(result.charAt(0)))){
+            return 1;
+        }
         for (int i = 0; i < result.length(); i++) {
-            char character = result.charAt(i);
+            /*
+            Условия проверки двух символов
+            */
+            if (i < result.length() - 1) {
 
-            //условие 3 согласных подряд
-            if (contains(Constants.CONSONANTS, String.valueOf(character))){
-                if (contains(Constants.CONSONANTS, String.valueOf(result.charAt(i+1)))){
-                    if (contains(Constants.CONSONANTS, String.valueOf(result.charAt(i+2)))){
-                        break;
-                    }
+                //Условие на наличие двух символов пунктуации подряд
+                if (contains(Constants.PUNCTUATION, String.valueOf(result.charAt(i))) &&
+                        contains(Constants.PUNCTUATION, String.valueOf(result.charAt(i + 1)))){
+                    return 1;
+
+                //Условие на наличие двух заглавных подряд
+                } else if (contains(Constants.RU_ALPHABET, String.valueOf(result.charAt(i))) &&
+                        contains(Constants.RU_ALPHABET, String.valueOf(result.charAt(i + 1)))) {
+                    return 1;
+
+                //Условие на пробел после знака препинания
+                } else if (contains(".,?!:;" , String.valueOf(result.charAt(i))) &&
+                        result.charAt(i+1) != ' ') {
+                    return 1;
+
+                //Условие на Заглавную букву сразу после прописной
+                }else if (contains(Constants.RU_LOW_ALPHABET, String.valueOf(result.charAt(i))) &&
+                        contains(Constants.RU_ALPHABET, String.valueOf(result.charAt(i+1)))){
+                    return 1;
                 }
             }
+            /*
+            Условия проверки трех символов
+            */
+            if (i < result.length() - 2) {
 
-            //Условие, если символ проходит проверку
-            else{
-                res = true;
+                //условие 3 согласных подряд
+                if (contains(Constants.CONSONANTS, String.valueOf(result.charAt(i)))) {
+                    if (contains(Constants.CONSONANTS, String.valueOf(result.charAt(i + 1)))) {
+                        if (contains(Constants.CONSONANTS, String.valueOf(result.charAt(i + 2)))) {
+                            return 1;
+                        }
+                    }
+
+                //Условие проверки трех одинаковых символов
+                }else if (result.charAt(i) == result.charAt(i+1) &&
+                        result.charAt(i)==result.charAt(i+2) &&
+                        contains(Constants.RU_LOW_ALPHABET, String.valueOf(result.charAt(i)))){
+                    return 1;
+                }
             }
         }
-        System.out.println("Возможная расшифровка: " + result);
-        return res;
+        return 0;
     }
 
     public static boolean contains(String str, String substr) {
